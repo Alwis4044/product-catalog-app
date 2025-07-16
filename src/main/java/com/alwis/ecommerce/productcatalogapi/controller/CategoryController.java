2138f3a375ec8eae64391e91c7a9202eada19ac6
Alwis Shaji <test1998.jm@gmail.com>
2025-07-16 18:47:51 +0530

    Second Commit

--- src/main/java/com/alwis/ecommerce/productcatalogapi/controller/CategoryController.java
+++ src/main/java/com/alwis/ecommerce/productcatalogapi/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package com.alwis.ecommerce.productcatalogapi.controller;
 
+import com.alwis.ecommerce.productcatalogapi.model.Product;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoryController {
+
+    @Id
+    private Long id;
+    private String name;
+    private List<Product> products;
 }
